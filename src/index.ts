3ec183825669b792f5584e1a9e5e1fa276ce1cf0
ru-461 <48552543+ru-461@users.noreply.github.com>
2023-09-21 20:37:53 +0900

    update: Replace messages with figlet

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Elysia } from "elysia";
+import figlet from "figlet";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => {
+  const body = figlet.textSync("Hello Elysia");
+  return body;
+}).listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
